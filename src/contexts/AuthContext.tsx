2ce7e77b70c1493834689a92d9cee69b795d37a1
guimamura <guilherme.imamura.fernandes@gmail.com>
2025-04-05 13:27:22 -0300

    fix(auth): ensure correct user type when loading from storage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,69 +1,32 @@
-import { createContext, useState, useEffect } from "react";
-import { useRouter } from "next/router";
-import {
-  getStorageItem,
-  setStorageItem,
-  removeStorageItem,
-} from "@/lib/storage";
+import { createContext, useState, ReactNode } from "react";
+import { getStorageItem } from "@/lib/storage";
 
 type User = {
   name: string;
   email: string;
+  password: string;
 };
 
 type AuthContextType = {
   user: User | null;
-  login: (email: string, password: string) => boolean;
-  register: (name: string, email: string, password: string) => void;
-  logout: () => void;
+  setUser: (user: User | null) => void;
 };
 
-export const AuthContext = createContext<AuthContextType>(
-  {} as AuthContextType
-);
+export const AuthContext = createContext({} as AuthContextType);
 
-export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const router = useRouter();
-  const [user, setUser] = useState<User | null>(null);
+type AuthProviderProps = {
+  children: ReactNode;
+};
 
-  useEffect(() => {
-    const storedUser = getStorageItem("user");
-    if (storedUser) {
-      setUser(storedUser);
-    }
-  }, []);
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const storageUser = getStorageItem<User>("user");
 
-  const login = (email: string, password: string) => {
-    const storedUser = getStorageItem("user");
-    if (
-      storedUser &&
-      storedUser.email === email &&
-      storedUser.password === password
-    ) {
-      setUser(storedUser);
-      setStorageItem("session-expire", Date.now() + 1000 * 60 * 5); // 5 minutos
-      router.push("/dashboard");
-      return true;
-    }
-    return false;
-  };
-
-  const register = (name: string, email: string, password: string) => {
-    const newUser = { name, email, password };
-    setStorageItem("user", newUser);
-    setUser(newUser);
-    setStorageItem("session-expire", Date.now() + 1000 * 60 * 5); // 5 minutos
-    router.push("/dashboard");
-  };
-
-  const logout = () => {
-    setUser(null);
-    removeStorageItem("session-expire");
-    router.push("/");
-  };
+  const [user, setUser] = useState<User | null>(
+    storageUser && Object.keys(storageUser).length > 0 ? storageUser : null
+  );
 
   return (
-    <AuthContext.Provider value={{ user, login, register, logout }}>
+    <AuthContext.Provider value={{ user, setUser }}>
       {children}
     </AuthContext.Provider>
   );
